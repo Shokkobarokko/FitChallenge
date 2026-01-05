96beb515f8616f331fd621a535ec9774c41eea57
Avetis <avetis.01@mail.ru>
2026-01-05 18:23:04 +0300

    Initial Commit

---
+++ FitChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitChallenge
+//
+//  Created by Аветис Парсаданян on 1/5/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
